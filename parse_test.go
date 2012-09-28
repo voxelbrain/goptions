877bb3560e8c665a17c83840c3d870777dc60338
Alexander Surma <surma@surmair.de>
2012-09-28 15:31:46 +0200

    Split testing files

--- parse_test.go
+++ parse_test.go
@@ -1,8 +1,6 @@
 package goptions
 
 import (
-	"os"
-	"reflect"
 	"testing"
 )
 
@@ -314,88 +312,4 @@ func TestParse_ObligatoryMutexGroup(t *testing.T) {
 	}
 }
 
-func TestParseTag_minimal(t *testing.T) {
-	var tag string
-	tag = `--name, -n, description='Some name'`
-	f, e := parseTag(tag)
-	if e != nil {
-		t.Fatalf("Tag parsing failed: %s", e)
-	}
-	expected := &Flag{
-		Long:        []string{"name"},
-		Short:       []string{"n"},
-		Description: "Some name",
-	}
-	if !reflect.DeepEqual(f, expected) {
-		t.Fatalf("Expected %#v, got %#v", expected, f)
-	}
-}
 
-func TestParseTag_more(t *testing.T) {
-	var tag string
-	tag = `--name, -n, description='Some name', mutexgroup='selector', obligatory`
-	f, e := parseTag(tag)
-	if e != nil {
-		t.Fatalf("Tag parsing failed: %s", e)
-	}
-	expected := &Flag{
-		Long:        []string{"name"},
-		Short:       []string{"n"},
-		Accumulate:  false,
-		Description: "Some name",
-		MutexGroup:  "selector",
-		Obligatory:  true,
-	}
-	if !reflect.DeepEqual(f, expected) {
-		t.Fatalf("Expected %#v, got %#v", expected, f)
-	}
-}
-
-func ExampleFlagSet_PrintHelp() {
-	var options struct {
-		Server    string `goptions:"-s, --server, obligatory, description='Server to connect to'"`
-		Password  string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
-		Verbosity int    `goptions:"-v, --verbose, accumulate, description='Set output threshold level'"`
-		Help      `goptions:"-h, --help, description='Show this help'"`
-
-		Verbs
-		Create struct {
-			Name      string `goptions:"-n, --name, obligatory, description='Name of the entity to be created'"`
-			Directory bool   `goptions:"--directory, mutexgroup='type', description='Create a directory'"`
-			File      bool   `goptions:"--file, mutexgroup='type', description='Create a file'"`
-		} `goptions:"create"`
-		Delete struct {
-			Name      string `goptions:"-n, --name, obligatory, description='Name of the entity to be deleted'"`
-			Directory bool   `goptions:"--directory, mutexgroup='type', description='Delete a directory'"`
-			File      bool   `goptions:"--file, mutexgroup='type', description='Delete a file'"`
-		} `goptions:"delete"`
-	}
-	args := []string{"--help"}
-	fs := NewFlagSet("goptions", &options)
-	err := fs.Parse(args)
-	if err == ErrHelpRequest {
-		fs.PrintHelp(os.Stderr)
-		return
-	} else if err != nil {
-		panic(err)
-	}
-
-	// Output:
-	// Usage: goptions [global options] <verb> [verb options]
-	//
-	// Global options:
-	//     -s, --server   Server to connect to (*)
-	//     -p, --password Don't prompt for password
-	//     -v, --verbose  Set output threshold level
-	//     -h, --help     Show this help
-	//
-	// Verbs:
-	//     create:
-	//         -n, --name      Name of the entity to be created (*)
-	//             --directory Create a directory
-	//             --file      Create a file
-	//     delete:
-	//         -n, --name      Name of the entity to be deleted (*)
-	//             --directory Delete a directory
-	//             --file      Delete a file
-}
---
+++ parsetag_test.go
@@ -0,0 +1,43 @@
+package goptions
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestParseTag_minimal(t *testing.T) {
+	var tag string
+	tag = `--name, -n, description='Some name'`
+	f, e := parseTag(tag)
+	if e != nil {
+		t.Fatalf("Tag parsing failed: %s", e)
+	}
+	expected := &Flag{
+		Long:        []string{"name"},
+		Short:       []string{"n"},
+		Description: "Some name",
+	}
+	if !reflect.DeepEqual(f, expected) {
+		t.Fatalf("Expected %#v, got %#v", expected, f)
+	}
+}
+
+func TestParseTag_more(t *testing.T) {
+	var tag string
+	tag = `--name, -n, description='Some name', mutexgroup='selector', obligatory`
+	f, e := parseTag(tag)
+	if e != nil {
+		t.Fatalf("Tag parsing failed: %s", e)
+	}
+	expected := &Flag{
+		Long:        []string{"name"},
+		Short:       []string{"n"},
+		Accumulate:  false,
+		Description: "Some name",
+		MutexGroup:  "selector",
+		Obligatory:  true,
+	}
+	if !reflect.DeepEqual(f, expected) {
+		t.Fatalf("Expected %#v, got %#v", expected, f)
+	}
+}
