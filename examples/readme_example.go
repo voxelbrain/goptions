aa334e9a9469b3f18ca91ad633d9bc723f7e7746
Alexander Surma <surma@surmair.de>
2012-10-04 19:01:27 +0200

    Adjust examples

--- examples/readme_example.go
+++ examples/readme_example.go
@@ -6,12 +6,13 @@ import (
 
 func main() {
 	options := struct {
-		Server        string `goptions:"-s, --server, description='Server to connect to'"`
-		Password      string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
-		Verbosity     int    `goptions:"-v, --verbose, description='Be verbose'"`
-		goptions.Help `goptions:"-h, --help, description='Show this help'"`
+		Server    string `goptions:"-s, --server, obligatory, description='Server to connect to'"`
+		Password  string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
+		Verbosity int    `goptions:"-v, --verbose, description='Be verbose'"`
+		Timeout   int    `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
+		Help      `goptions:"-h, --help, description='Show this help'"`
 
-		goptions.Verbs
+		Verbs
 		Create struct {
 			Name      string `goptions:"-n, --name, obligatory, description='Name of the entity to be created'"`
 			Directory bool   `goptions:"--directory, mutexgroup='type', description='Create a directory'"`
@@ -22,8 +23,8 @@ func main() {
 			Directory bool   `goptions:"--directory, mutexgroup='type', description='Delete a directory'"`
 			File      bool   `goptions:"--file, mutexgroup='type', description='Delete a file'"`
 		} `goptions:"delete"`
-	}{ // Default values go here
-		Server: "localhost",
+	}{ // Default values goes here
+		Timeout: 10,
 	}
 	goptions.ParseAndFail(&options)
 }
