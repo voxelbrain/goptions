82ce8c675d2cf5378b9b52697318c7991bf1ffc5
Alexander Surma <surma@surmair.de>
2012-10-04 19:00:13 +0200

    Adjust test

--- examples_test.go
+++ examples_test.go
@@ -6,10 +6,11 @@ import (
 )
 
 func ExampleFlagSet_PrintHelp() {
-	var options struct {
+	options := struct {
 		Server    string `goptions:"-s, --server, obligatory, description='Server to connect to'"`
 		Password  string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
 		Verbosity int    `goptions:"-v, --verbose, description='Be verbose'"`
+		Timeout   int    `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
 		Help      `goptions:"-h, --help, description='Show this help'"`
 
 		Verbs
@@ -23,6 +24,8 @@ func ExampleFlagSet_PrintHelp() {
 			Directory bool   `goptions:"--directory, mutexgroup='type', description='Delete a directory'"`
 			File      bool   `goptions:"--file, mutexgroup='type', description='Delete a file'"`
 		} `goptions:"delete"`
+	}{ // Default values goes here
+		Timeout: 10,
 	}
 	args := []string{"--help"}
 	fs := NewFlagSet("goptions", &options)
@@ -41,6 +44,7 @@ func ExampleFlagSet_PrintHelp() {
 	//     -s, --server   Server to connect to (*)
 	//     -p, --password Don't prompt for password
 	//     -v, --verbose  Be verbose
+	//     -t, --timeout  Connection timeout in seconds (default: 10)
 	//     -h, --help     Show this help
 	//
 	// Verbs:
