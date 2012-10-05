eb75af9a874ae403b2e8a2ee470aa5c7b5d8bdb9
Alexander Surma <surma@surmair.de>
2012-10-05 15:29:56 +0200

    Update example codes

--- TODO.md
+++ TODO.md
@@ -1,2 +1,6 @@
 TODO
 ====
+
+* Possibility to make selection of a verb obligatory
+* Feedback which verb has been selected
+* Represent MutexGroups in HelpOutput
--- examples/readme_example.go
+++ examples/readme_example.go
@@ -2,26 +2,24 @@ package main
 
 import (
 	"github.com/voxelbrain/goptions"
+	"os"
 )
 
 func main() {
 	options := struct {
-		Server    string `goptions:"-s, --server, obligatory, description='Server to connect to'"`
-		Password  string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
-		Verbosity int    `goptions:"-v, --verbose, description='Be verbose'"`
-		Timeout   int    `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
-		Help      `goptions:"-h, --help, description='Show this help'"`
+		Server        string `goptions:"-s, --server, obligatory, description='Server to connect to'"`
+		Password      string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
+		Timeout       int    `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
+		goptions.Help `goptions:"-h, --help, description='Show this help'"`
 
-		Verbs
-		Create struct {
-			Name      string `goptions:"-n, --name, obligatory, description='Name of the entity to be created'"`
-			Directory bool   `goptions:"--directory, mutexgroup='type', description='Create a directory'"`
-			File      bool   `goptions:"--file, mutexgroup='type', description='Create a file'"`
-		} `goptions:"create"`
+		goptions.Verbs
+		Execute struct {
+			Command string   `goptions:"--command, mutexgroup='input', description='Command to exectute', obligatory"`
+			Script  *os.File `goptions:"--script, mutexgroup='input', description='Script to execture', create, wronly, append"`
+		} `goptions:"execute"`
 		Delete struct {
-			Name      string `goptions:"-n, --name, obligatory, description='Name of the entity to be deleted'"`
-			Directory bool   `goptions:"--directory, mutexgroup='type', description='Delete a directory'"`
-			File      bool   `goptions:"--file, mutexgroup='type', description='Delete a file'"`
+			Path  string `goptions:"-n, --name, obligatory, description='Name of the entity to be deleted'"`
+			Force bool   `goptions:"-f, --force, description='Force removal'"`
 		} `goptions:"delete"`
 	}{ // Default values goes here
 		Timeout: 10,
