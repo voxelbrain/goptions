76029649f8c316b4f5f80a039585fae95773e4a4
Alexander Surma <surma@surmair.de>
2013-02-25 18:37:07 +0100

    Update example

--- examples_test.go
+++ examples_test.go
@@ -3,26 +3,27 @@ package goptions
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func ExampleFlagSet_PrintHelp() {
 	options := struct {
-		Server   string `goptions:"-s, --server, obligatory, description='Server to connect to'"`
-		Password string `goptions:"-p, --password, description='Don\\'t prompt for password'"`
-		Timeout  int    `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
-		Help     `goptions:"-h, --help, description='Show this help'"`
+		Server   string        `goptions:"-s, --server, obligatory, description='Server to connect to'"`
+		Password string        `goptions:"-p, --password, description='Don\\'t prompt for password'"`
+		Timeout  time.Duration `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
+		Help     Help          `goptions:"-h, --help, description='Show this help'"`
 
 		Verbs
 		Execute struct {
 			Command string   `goptions:"--command, mutexgroup='input', description='Command to exectute', obligatory"`
-			Script  *os.File `goptions:"--script, mutexgroup='input', description='Script to execture', create, wronly, append"`
+			Script  *os.File `goptions:"--script, mutexgroup='input', description='Script to exectute', rdonly"`
 		} `goptions:"execute"`
 		Delete struct {
 			Path  string `goptions:"-n, --name, obligatory, description='Name of the entity to be deleted'"`
 			Force bool   `goptions:"-f, --force, description='Force removal'"`
 		} `goptions:"delete"`
 	}{ // Default values goes here
-		Timeout: 10,
+		Timeout: 10 * time.Second,
 	}
 
 	args := []string{"--help"}
@@ -41,7 +42,7 @@ func ExampleFlagSet_PrintHelp() {
 	// Global options:
 	//     -s, --server   Server to connect to (*)
 	//     -p, --password Don't prompt for password
-	//     -t, --timeout  Connection timeout in seconds (default: 10)
+	//     -t, --timeout  Connection timeout in seconds (default: 10s)
 	//     -h, --help     Show this help
 	//
 	// Verbs:
